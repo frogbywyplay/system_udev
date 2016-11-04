14d29c664e72dad6a5dce68baf1fd781b44217a7
Thibaut Perrot <tperrot@wyplay.com>
2016-11-04 09:45:07 +0100

    add space into config.h

--- udev/config.h
+++ udev/config.h
@@ -35,9 +35,9 @@ extern const struct udevadm_cmd udevadm_settle;
  * Which commands you want to execute
  */
 static const struct mknods_arg mknods_arguments[ARGUMENTS_SIZE] = {
-  (const struct mknods_arg){ &udevadm_trigger, 3, (const char *[]){"trigger","--action=add", "--type=subsystems"} },
-  (const struct mknods_arg){ &udevadm_trigger, 3, (const char *[]){"trigger","--action=add", "--type=devices"} },
-  (const struct mknods_arg){ &udevadm_settle,  2, (const char *[]){"settle","--timeout=60" } }
+  (const struct mknods_arg){ &udevadm_trigger, 3, (const char *[]){"trigger", "--action=add", "--type=subsystems"} },
+  (const struct mknods_arg){ &udevadm_trigger, 3, (const char *[]){"trigger", "--action=add", "--type=devices"} },
+  (const struct mknods_arg){ &udevadm_settle,  2, (const char *[]){"settle", "--timeout=60"} }
 };
 
 #endif
